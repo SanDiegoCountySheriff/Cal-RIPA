0b0c83c2915221be8054200046be4324a70854aa
Steve Pietrek <spietrek@gmail.com>
2021-04-07 21:20:58 -0400

    add first-cut form support

---
+++ UI/src/components/molecules/RipaStopReason.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <!-- <v-form ref="stopReason" v-model="valid" lazy-validation> -->
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Item"
+      required
+    ></v-select>
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox>
+
+    <!-- <v-btn :disabled="!valid" color="success" class="tw-mr-4" @click="validate">
+      Validate
+    </v-btn>
+
+    <v-btn color="error" class="tw-mr-4" @click="reset"> Reset Form </v-btn>
+
+    <v-btn color="warning" class="tw-mr-4" @click="resetValidation">
+      Reset Validation
+    </v-btn>
+
+    <v-btn :disabled="!valid" color="primary" class="tw-mr-4" @click="submit">
+      submit
+    </v-btn> -->
+    <!-- </v-form> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ripa-stop-reason',
+
+  data() {
+    return {
+      valid: true,
+      name: this.value.name || '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      select: null,
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+      checkbox: false,
+    }
+  },
+
+  methods: {
+    validate() {
+      this.$refs.stopReason.validate()
+    },
+
+    reset() {
+      this.$refs.stopReason.reset()
+    },
+
+    resetValidation() {
+      this.$refs.stopReason.resetValidation()
+    },
+
+    submit() {
+      this.$refs.stopReason.validate()
+      if (!this.valid) {
+        return
+      }
+      this.$emit('input', {
+        name: this.name,
+        year: this.email,
+        select: this.select,
+        checkbox: this.checkbox,
+      })
+    },
+  },
+
+  props: {
+    value: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
