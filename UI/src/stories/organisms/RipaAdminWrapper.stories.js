248b32a1bd48571edbb29426a3629375775a3884
Steve Pietrek <spietrek@gmail.com>
2021-04-08 13:23:13 -0400

    create first-cut admin wrapper

---
+++ UI/src/stories/organisms/RipaAdminWrapper.stories.js
@@ -0,0 +1,29 @@
+import RipaAdminWrapper from '@/components/organisms/RipaAdminWrapper'
+import { beats } from '../data/beats'
+import { cities } from '../data/cities'
+import { STATES } from '@/constants/states'
+import { COUNTIES } from '@/constants/counties'
+import { schools } from '../data/schools'
+import { statutes } from '../data/statutes'
+
+export default {
+  title: 'Organisms/RipaAdminWrapper',
+  component: RipaAdminWrapper,
+  parameters: {},
+}
+
+export const basic = () => ({
+  components: { RipaAdminWrapper },
+  data() {
+    return {
+      beats: beats,
+      cities: cities,
+      counties: COUNTIES,
+      schools: schools,
+      states: STATES,
+      statutes: statutes,
+    }
+  },
+  template:
+    '<ripa-admin-wrapper :beats="beats" :cities="cities" :counties="counties" :schools="schools" :states="states" :statutes="statutes"></ripa-admin-wrapper>',
+})
