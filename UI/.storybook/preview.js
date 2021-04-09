055220e11076fe6f57ef258b8c6170d797c98c60
Steve Pietrek <spietrek@gmail.com>
2021-04-09 13:48:11 -0400

    add custom viewport

--- UI/.storybook/preview.js
+++ UI/.storybook/preview.js
@@ -5,9 +5,19 @@ import '@/plugins/tailwind'
 import '@mdi/font/css/materialdesignicons.css'
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
+const customViewports = {
+  panasonicCf33: {
+    name: 'Panasonic CF-33',
+    styles: {
+      width: '1440px',
+      height: '2160px',
+    },
+  },
+}
+
 addParameters({
   viewport: {
-    viewports: INITIAL_VIEWPORTS,
+    viewports: { ...INITIAL_VIEWPORTS, ...customViewports },
   },
 })
 
