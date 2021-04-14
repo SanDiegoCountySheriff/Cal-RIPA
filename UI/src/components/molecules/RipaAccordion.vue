d4a63f3fa11299313ce74c990692e0acbb74ec33
Steve Pietrek <spietrek@gmail.com>
2021-04-13 20:29:29 -0400

    update stops components

--- UI/src/components/molecules/RipaAccordion.vue
+++ UI/src/components/molecules/RipaAccordion.vue
@@ -1,9 +1,10 @@
 <template>
   <v-expansion-panels>
     <v-expansion-panel v-for="(item, i) in items" :key="i">
-      <v-expansion-panel-header> {{ item.title }} </v-expansion-panel-header>
+      <v-expansion-panel-header> {{ item.time }} </v-expansion-panel-header>
       <v-expansion-panel-content>
-        {{ item.content }}
+        <div class="tw-text-xs tw-break-words">{{ item.jsonStop }}</div>
+        <v-btn color="secondary"> Duplicate </v-btn>
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
