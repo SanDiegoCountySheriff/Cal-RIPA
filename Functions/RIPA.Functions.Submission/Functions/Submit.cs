9fb366f4209cd59e4849ded7c41047d7ed0daf7a
LoganPopeInsight <60166524+LoganPopeInsight@users.noreply.github.com>
2021-04-20 15:11:46 -0700

    Feature/backend services (#46)
    * Add openapi spec to Domain, stop model, consistency
    
    * bug causing build to fail
    
    * openapi for text analytics
    
    * openapi/swagger for remaining functions

--- Functions/RIPA.Functions.Submission/Functions/Submit.cs
+++ Functions/RIPA.Functions.Submission/Functions/Submit.cs
@@ -1,17 +1,23 @@
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.OpenApi.Models;
+using System.Net;
+using System.Threading.Tasks;
+
 
 namespace RIPA.Functions.Submission.Functions
 {
     public static class Submission
     {
         [FunctionName("Submit")]
+        [OpenApiOperation(operationId: "Submit", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "DOJ Submit Success")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
         {
