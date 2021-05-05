f3c0eb901ccfa51f0df8af435650b02fef9ef0ec
spietrek <spietrek@gmail.com>
2021-05-05 06:27:39 -0400

    add validation rules

--- UI/src/components/molecules/RipaStopDate.vue
+++ UI/src/components/molecules/RipaStopDate.vue
@@ -10,6 +10,7 @@
             <ripa-date-picker
               v-model="model.stopDate.date"
               label="Date of Stop"
+              :rules="dateRules"
               @input="handleInput"
             >
             </ripa-date-picker>
@@ -21,6 +22,7 @@
             <ripa-time-picker
               v-model="model.stopDate.time"
               label="Time of Stop"
+              :rules="timeRules"
               @input="handleInput"
             >
             </ripa-time-picker>
@@ -34,6 +36,7 @@
             :items="durationItems"
             itemText="name"
             itemValue="value"
+            :rules="durationRules"
             @input="handleInput"
           >
           </ripa-select>
@@ -62,6 +65,7 @@ import RipaSwitch from '@/components/atoms/RipaSwitch'
 import RipaTimePicker from '@/components/atoms/RipaTimePicker'
 import { format } from 'date-fns'
 import { DURATIONS } from '@/constants/form'
+import { dateWithinLastHours } from '@/utilities/dates'
 
 export default {
   name: 'ripa-stop-date',
@@ -78,6 +82,19 @@ export default {
     return {
       valid: true,
       durationItems: DURATIONS,
+      dateRules: [
+        v => !!v || 'A date is required',
+        v =>
+          (v && this.isValidDateTime()) ||
+          'Date and Time must be within the past 24 hours',
+      ],
+      timeRules: [
+        v => !!v || 'A time is required',
+        v =>
+          (v && this.isValidDateTime()) ||
+          'Date and Time must be within the past 24 hours',
+      ],
+      durationRules: [v => !!v || 'A duration is required'],
       viewModel: {
         stopDate: {
           date: this.value?.stopDate?.date || format(new Date(), 'yyyy-MM-dd'),
@@ -101,6 +118,12 @@ export default {
   methods: {
     handleInput() {
       this.$emit('input', this.viewModel)
+    },
+
+    isValidDateTime() {
+      const dateStr = this.viewModel.stopDate.date
+      const timeStr = this.viewModel.stopDate.time
+      return dateWithinLastHours(dateStr, timeStr, 24)
     },
   },
 
