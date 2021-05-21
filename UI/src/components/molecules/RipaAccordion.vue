5c44dfe83c7adb35b59de38fd5348d6ad6975ef5
Todd Darling <todd.darling@insight.com>
2021-05-21 09:11:14 -0400

    Adjust column spacing for edit button

--- UI/src/components/molecules/RipaAccordion.vue
+++ UI/src/components/molecules/RipaAccordion.vue
@@ -8,7 +8,7 @@
       <v-expansion-panel-header>
         <v-row>
           <v-col cols="12">
-            Stop #{{ item.ori }}
+            <h3>Stop #{{ item.id }}</h3>
             <span class="stopAccordionWrapper--dateTime text--secondary">
               {{ formatStopDate(item.stopDateTime) }}
             </span>
@@ -19,49 +19,49 @@
         <div class="tw-text-xs tw-break-words">{{ item.jsonStop }}</div>
         <div class="stopAccordionWrapper--detailSection">
           <v-row align="center">
-            <v-col cols="5"> DETAILS </v-col>
-            <v-col cols="3">
+            <v-col cols="3">DETAILS </v-col>
+            <v-col cols="5">
               <v-btn color="secondary"> Edit </v-btn>
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="5"> Date </v-col>
+            <v-col cols="3"> Date </v-col>
             <v-col cols="3">
               {{ formatStopDate(item.stopDateTime) }}
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="5"> Time </v-col>
+            <v-col cols="3"> Time </v-col>
             <v-col cols="3">
               {{ item.time }}
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="5"> Duration </v-col>
+            <v-col cols="3"> Duration </v-col>
             <v-col cols="3">
               {{ item.stopDuration }}
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="5"> Block </v-col>
+            <v-col cols="3"> Block </v-col>
             <v-col cols="3">
               {{ item.location.blockNumber }}
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="5"> Street </v-col>
+            <v-col cols="3"> Street </v-col>
             <v-col cols="3">
               {{ item.location.streetName }}
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="5"> City </v-col>
+            <v-col cols="3"> City </v-col>
             <v-col cols="3">
               {{ item.location.streetname }}
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="5"> Beat </v-col>
+            <v-col cols="3"> Beat </v-col>
             <v-col cols="3">
               {{ item.location.streetname }}
             </v-col>
@@ -81,13 +81,13 @@
             class=""
           >
             <v-row align="center">
-              <v-col cols="5"> PERSON {{ i + 1 }} </v-col>
+              <v-col cols="3"> PERSON {{ i + 1 }} </v-col>
               <v-col cols="3">
                 <v-btn color="secondary"> Edit</v-btn>
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Perceived Race</v-col>
+              <v-col cols="3">Perceived Race</v-col>
               <v-col cols="3">
                 <span
                   class="listItems"
@@ -98,31 +98,31 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Perceived Age</v-col>
+              <v-col cols="3">Perceived Age</v-col>
               <v-col cols="3">
                 {{ person.perceivedAge }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Perceived Gender</v-col>
+              <v-col cols="3">Perceived Gender</v-col>
               <v-col cols="3">
                 {{ person.perceivedGender }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Gender Nonconforming</v-col>
+              <v-col cols="3">Gender Nonconforming</v-col>
               <v-col cols="3">
                 {{ person.genderNonconforming ? 'Yes' : 'No' }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Perceived LGBT</v-col>
+              <v-col cols="3">Perceived LGBT</v-col>
               <v-col cols="3">
                 {{ person.perceivedLgbt ? 'Yes' : 'No' }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Perceived Disability</v-col>
+              <v-col cols="3">Perceived Disability</v-col>
               <v-col cols="3">
                 <span
                   class="listItems"
@@ -135,7 +135,7 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Reason for Stop</v-col>
+              <v-col cols="3">Reason for Stop</v-col>
               <v-col cols="3">
                 <span
                   class="listItems"
@@ -152,13 +152,13 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Comments on Stop</v-col>
+              <v-col cols="3">Comments on Stop</v-col>
               <v-col cols="3">
                 {{ person.reasonForStopExplanation }}
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Actions Taken</v-col>
+              <v-col cols="3">Actions Taken</v-col>
               <v-col cols="3">
                 <span
                   class="listItems"
@@ -171,7 +171,7 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Contraband Discovered</v-col>
+              <v-col cols="3">Contraband Discovered</v-col>
               <v-col cols="3">
                 <span
                   class="listItems"
@@ -184,7 +184,7 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="5">Result of Stop</v-col>
+              <v-col cols="3">Result of Stop</v-col>
               <v-col cols="3">
                 <span
                   class="listItems"
@@ -225,6 +225,10 @@ export default {
 <style lang="scss">
 .stopAccordionWrapper {
   margin: 10px 0;
+
+  h3 {
+    font-size: 18px;
+  }
 
   .stopAccordionWrapper--dateTime {
     display: block;
