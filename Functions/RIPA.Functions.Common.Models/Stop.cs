f231c447bd36ded4c6b6d2b741e25616227b4584
LoganPopeInsight <60166524+LoganPopeInsight@users.noreply.github.com>
2021-05-21 15:22:40 -0700

    add officer name to stop (#116)

--- Functions/RIPA.Functions.Common.Models/Stop.cs
+++ Functions/RIPA.Functions.Common.Models/Stop.cs
@@ -62,6 +62,7 @@ namespace RIPA.Functions.Common.Models
         public string Ori { get; set; }
         public string Agency { get; set; }
         public string OfficerId { get; set; }
+        public string OfficerName { get; set; }
         public string ExpYears { get; set; }
         public OfficerAssignment OfficerAssignment { get; set; }
         public string Date { get; set; }
