8ddbcb828890bacbd0dde64aadcaf0de8358d295
spietrek <spietrek@gmail.com>
2021-05-23 19:44:28 -0400

    create edit stop mixin

---
+++ UI/src/components/mixins/RipaEditStopMixin.vue
@@ -0,0 +1,19 @@
+<script>
+import { apiStopToFullStop, fullStopToStop } from '@/utilities/stop'
+import router from '@/router'
+
+export default {
+  methods: {
+    handleEditStop(apiStop) {
+      const fullStop = apiStopToFullStop(apiStop)
+      const stop = fullStopToStop(fullStop)
+      localStorage.removeItem('ripa_form_current_user')
+      localStorage.setItem('ripa_form_step_index', '6')
+      localStorage.setItem('ripa_form_editing', '1')
+      localStorage.setItem('ripa_form_stop', JSON.stringify(stop))
+      localStorage.setItem('ripa_form_full_stop', JSON.stringify(fullStop))
+      router.push('/')
+    },
+  },
+}
+</script>
