eb40582d2291d762938c2b15c5e185c1a1a290ce
LoganPopeInsight <60166524+LoganPopeInsight@users.noreply.github.com>
2021-05-25 14:59:42 -0700

    Feature/lpope stops summary (#124)
    * summary of stops
    
    * GetErrorCodes api

--- Functions/RIPA.Functions.Stop/Functions/GetErrorCodes.cs
+++ Functions/RIPA.Functions.Stop/Functions/GetErrorCodes.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -10,24 +11,32 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
+using RIPA.Functions.Common.Services.Stop.CosmosDb.Contracts;
 
 namespace RIPA.Functions.Stop.Functions
 {
-    public static class GetErrorCodes
+    public class GetErrorCodes
     {
+        private IStopCosmosDbService _stopCosmosDbService;
+        public GetErrorCodes(IStopCosmosDbService stopCosmosDbService)
+        {
+            _stopCosmosDbService = stopCosmosDbService;
+        }
+
         [FunctionName("GetErrorCodes")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiParameter(name: "Search", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "Type ahead error search")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "Return a list of errors, maybe object with code and message properties")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(List<string>), Description = "Return a list of errors, maybe object with code and message properties")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
         {
-            log.LogInformation("TODO");
-            //Get all error codes based upon input string as type ahead functionality
-            //TODO
-            return new OkObjectResult(true);
+            log.LogInformation("Get - GetErrorCodes requested.");
+
+            var inputText = req.Query["Search"];
+            var response = await _stopCosmosDbService.GetErrorCodes(inputText);
+            
+            return new OkObjectResult(response);
         }
     }
 }
