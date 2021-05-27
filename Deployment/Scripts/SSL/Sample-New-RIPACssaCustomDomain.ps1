181ae8f4210404c1360fa3c4d159cd83e7ffecbb
Les McWhirter <les.mcwhirter@insight.com>
2021-05-27 15:18:54 -0700

    Wrote script to configure custom domain

---
+++ Deployment/Scripts/SSL/Sample-New-RIPACssaCustomDomain.ps1
@@ -0,0 +1,27 @@
+﻿Import-Module .\New-RIPACssaCustomDomain.psm1 -Force 
+
+$Subscription = "********-****-****-****-************"
+$ResourceGroupName = "cssa-shared-rg"
+$CdnProfileName = "cssa-cloud-cdn"
+$DnsRootZone = "cssa.cloud"
+
+$AgencyAbrieviation = "salsa"
+$ApplicationName = "green"
+
+$KeyVaultName = "cssa-shared-kv"
+$SecretName = "star-cssa-cloud"
+$SecretVersion = "Latest"
+
+$WebContentUrl = "staticwebcontent.z13.web.core.windows.net"
+
+New-RIPACssaCustomDomain `
+    -Subscription $Subscription `
+    -ResourceGroupName $ResourceGroupName `
+    -CdnProfileName $CdnProfileName `
+    -DnsRootZone $DnsRootZone `
+    -AgencyAbrieviation $AgencyAbrieviation `
+    -ApplicationName $ApplicationName `
+    -KeyVaultName $KeyVaultName `
+    -SecretName $SecretName `
+    -SecretVersion $SecretVersion `
+    -WebContentUrl $WebContentUrl 
