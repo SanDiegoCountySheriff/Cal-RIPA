3ec17dc0e7dbcf6826a1292248ccc33c39280dfe
spietrek <spietrek@gmail.com>
2021-06-01 11:16:23 -0400

    update verbiage

--- UI/src/components/molecules/RipaFormStep3.vue
+++ UI/src/components/molecules/RipaFormStep3.vue
@@ -60,8 +60,8 @@ export default {
       const piiFound = this.viewModel.stopReason?.reasonForStopPiiFound || false
       if (piiFound) {
         this.$confirm({
-          title: 'Confirm Cancel',
-          message: `This page contains personally identifying information. Are you sure you want to continue?`,
+          title: 'Confirm Continue',
+          message: `This page may contain personally identifying information. Are you sure you want to continue?`,
           button: {
             no: 'No',
             yes: 'Yes',
--- UI/src/components/molecules/RipaFormStep4.vue
+++ UI/src/components/molecules/RipaFormStep4.vue
@@ -63,8 +63,8 @@ export default {
         this.viewModel.actionsTaken?.basisForSearchPiiFound || false
       if (piiFound) {
         this.$confirm({
-          title: 'Confirm Cancel',
-          message: `This page contains personally identifying information. Are you sure you want to continue?`,
+          title: 'Confirm Continue',
+          message: `This page may contain personally identifying information. Are you sure you want to continue?`,
           button: {
             no: 'No',
             yes: 'Yes',
