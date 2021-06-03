0fc81626a102bd3345585d29242f94e190898e23
LoganPopeInsight <Logan.Pope@Insight.com>
2021-06-03 15:06:51 -0700

    add submit search

---
+++ Functions/RIPA.Functions.Common.Models/StopQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace RIPA.Functions.Common.Models
+{
+    public class StopQuery
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool? IsPII { get; set; }
+        public string ErrorCode { get; set; }
+        public string Status { get; set; }
+        public bool? IsSubmitted { get; set; }
+        public string OfficerId { get; set; }
+        public int Limit { get; set; }
+        public int Offset { get; set; }
+
+    }
+}
