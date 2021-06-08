5f1c37e0b0c21d5ec402641e7b9e17d8878da5af
LoganPopeInsight <60166524+LoganPopeInsight@users.noreply.github.com>
2021-06-08 16:07:50 -0700

    Audit Stop changes (#159)

---
+++ Functions/RIPA.Functions.Stop/Functions/TriggerStopAudit.cs
@@ -0,0 +1,39 @@
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RIPA.Functions.Stop.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RIPA.Functions.Stop.Functions
+{
+    public class TriggerStopAudit
+    {
+        private readonly IStopAuditCosmosDbService _stopAuditCosmosDbService;
+
+        public TriggerStopAudit(IStopAuditCosmosDbService stopAuditCosmosDbService)
+        {
+            _stopAuditCosmosDbService = stopAuditCosmosDbService;
+        }
+
+        [FunctionName("TriggerStopAudit")]
+        public async Task Run(
+            [CosmosDBTrigger(databaseName: "ripastops", collectionName: "stop",ConnectionStringSetting = "CosmosConnectionString", LeaseCollectionName = "leases", CreateLeaseCollectionIfNotExists = true)]
+            IReadOnlyList<Document> input, ILogger log
+        )
+        {
+            if (input != null && input.Count > 0)
+            {
+                foreach (Document document in input)
+                {
+                    Common.Models.Stop stop = JsonConvert.DeserializeObject<Common.Models.Stop>(document.ToString());
+                    stop.id = $"{stop.id}-{DateTime.UtcNow.ToString("yyyyMMddHHmmss")}";
+                    await _stopAuditCosmosDbService.UpdateStopAuditAsync(stop.id, stop);
+                    log.LogInformation($"archived stop id: {document.Id}");
+                }
+            }
+        }
+    }
+}
