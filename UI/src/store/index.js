26eff9c4fa089a5610d43e9fdf5f1db9bf1dece2
Steve Pietrek <spietrek@gmail.com>
2021-06-08 15:50:45 -0400

    fix checkPii issue where end point was changed

--- UI/src/store/index.js
+++ UI/src/store/index.js
@@ -362,7 +362,7 @@ export default new Vuex.Store({
         .then(response => {
           const data = response.data
           commit('updatePiiDate')
-          return data.entities.length > 0
+          return data && data.piiEntities && data.piiEntities.length > 0
         })
         .catch(error => {
           console.log('There was an error checking for PII.', error)
