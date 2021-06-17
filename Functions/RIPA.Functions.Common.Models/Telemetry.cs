adabe30664565a1c4f09d83c84b38821425ecfcd
LoganPopeInsight <60166524+LoganPopeInsight@users.noreply.github.com>
2021-06-17 12:41:46 -0700

    Add telemetry object to stop (#181)

---
+++ Functions/RIPA.Functions.Common.Models/Telemetry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RIPA.Functions.Common.Models
+{
+    public class Telemetry
+    {
+        public object Template { get; set; }
+        public bool FormCached { get; set; }
+        public Liststeptrace[] ListStepTrace { get; set; }
+        public string LookupCacheDate { get; set; }
+        public bool PullFromReasonCode { get; set; }
+    }
+
+    public class Liststeptrace
+    {
+        public int Index { get; set; }
+        public DateTime StartTimeStamp { get; set; }
+        public DateTime EndTimeStamp { get; set; }
+    }
+
+
+}
