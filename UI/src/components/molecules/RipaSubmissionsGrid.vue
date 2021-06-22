0c4a37a4437c68c4a725e984ca60a92ee3e05df6
Todd Darling <todd.darling@insight.com>
2021-06-22 10:06:32 -0400

    Fix filtering not resetting pagination correctly on submissions

--- UI/src/components/molecules/RipaSubmissionsGrid.vue
+++ UI/src/components/molecules/RipaSubmissionsGrid.vue
@@ -246,6 +246,7 @@ export default {
           order: this.sortDesc ? 'Desc' : 'Asc',
         },
       }
+      this.currentPage = 1
       this.$emit('handleFilter', filterData)
     },
     getColumnSortName() {
