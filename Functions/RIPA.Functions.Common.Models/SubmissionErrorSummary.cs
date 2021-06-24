b8ff5448a7720fcd0d8dc4539818c902f1a0c760
LoganPopeInsight <60166524+LoganPopeInsight@users.noreply.github.com>
2021-06-24 13:07:57 -0700

    Feature/lpope submission summary enhancements (#219)
    * addition of stop dates max and min to summary
    
    * refactor submission summary to use db rather than server side

---
+++ Functions/RIPA.Functions.Common.Models/SubmissionErrorSummary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RIPA.Functions.Common.Models
+{
+    public class SubmissionErrorSummary
+    {
+        public int Count { get; set; }
+        public string Code { get; set; }
+    }
+}
