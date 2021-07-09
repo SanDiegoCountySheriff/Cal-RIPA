7453252fd7318c2fba32cc5385f3acf7bd283d5a
LoganPopeInsight <Logan.Pope@Insight.com>
2021-07-09 09:58:01 -0700

    bug in error codes

--- Functions/RIPA.Functions.Stop/Functions/GetErrorCodes.cs
+++ Functions/RIPA.Functions.Stop/Functions/GetErrorCodes.cs
@@ -30,7 +30,7 @@ namespace RIPA.Functions.Stop.Functions
         [OpenApiSecurity("Bearer", SecuritySchemeType.OAuth2, Name = "Bearer Token", In = OpenApiSecurityLocationType.Header, Flows = typeof(RIPAAuthorizationFlow))]
         [OpenApiParameter(name: "Ocp-Apim-Subscription-Key", In = ParameterLocation.Header, Required = true, Type = typeof(string), Description = "Ocp-Apim-Subscription-Key")]
         [OpenApiParameter(name: "Search", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "Type ahead error search")]
-        [OpenApiParameter(name: "SubmissionId", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The Submission Id to filter errors by")]
+        [OpenApiParameter(name: "SubmissionId", In = ParameterLocation.Query, Required = false, Type = typeof(string), Description = "The Submission Id to filter errors by")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(List<string>), Description = "Return a list of errors, maybe object with code and message properties")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
