629c87850b8bfa180ccf9c99cc8a6e3c57756e88
Les McWhirter <les.mcwhirter@insight.com>
2021-07-26 10:14:24 -0700

    FIxed "IsEdited" logic

--- UI/src/components/molecules/RipaSubmission.vue
+++ UI/src/components/molecules/RipaSubmission.vue
@@ -99,7 +99,7 @@
             <div class="stopError" v-html="item.error"></div>
           </template>
           <template v-slot:item.edited="{ item }">
-            {{ item.listSubmission.length ? 'Yes' : 'No' }}
+            {{ item.IsEdited ? 'Yes' : 'No' }}
           </template>
           <template v-slot:no-data>
             <div>No Stops Found in This Submission</div>
