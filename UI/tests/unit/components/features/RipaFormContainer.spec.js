d6f2fbbb57de5e42ca59bf6a4a799e713ed27b51
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2021-12-21 14:55:39 -0800

    Update RipaFormContainer.spec.js

--- UI/tests/unit/components/features/RipaFormContainer.spec.js
+++ UI/tests/unit/components/features/RipaFormContainer.spec.js
@@ -1,20 +1,48 @@
 import RipaFormContainer from '@/components/features/RipaFormContainer.vue'
-import { mount } from '@vue/test-utils'
-import { mapGetters } from 'vuex'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
 import Vuetify from 'vuetify'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('Ripa Form Container', () => {
   let vuetify
+  let store
+  let actions
+  let getters
   let wrapper = null
 
   beforeEach(() => {
     vuetify = new Vuetify()
+    actions = {}
+    getters = {
+      mappedGpsLocationAddress: jest.fn(),
+      mappedFormBeats: jest.fn(),
+      mappedFormCountyCities: jest.fn(),
+      displayBeatInput: jest.fn(),
+      displayDebugger: jest.fn(),
+      isAuthenticated: jest.fn(),
+      isOnlineAndAuthenticated: jest.fn(),
+      mappedFormNonCountyCities: jest.fn(),
+      mappedFormSchools: jest.fn(),
+      mappedFormStatutes: jest.fn(),
+      mappedUser: jest.fn(() => {
+        return { agency: 'SDSD' }
+      }),
+      stopTemplates: jest.fn(),
+    }
+    store = new Vuex.Store({
+      actions,
+      getters,
+    })
   })
 
   const factory = propsData => {
     return mount(RipaFormContainer, {
       vuetify,
-      mapGetters,
+      store,
+      localVue,
       propsData: {
         ...propsData,
       },
