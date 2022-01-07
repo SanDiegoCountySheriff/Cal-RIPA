89c9ff2be80d463b3fa836eb8bcb0ecb1b5fec9c
Jake Kellas <jacob.kellas@sdsheriff.org>
2022-01-07 15:24:39 -0800

    Update RipaActionsTaken.spec.js

--- UI/tests/unit/components/molecules/RipaActionsTaken.spec.js
+++ UI/tests/unit/components/molecules/RipaActionsTaken.spec.js
@@ -1,13 +1,17 @@
 import RipaActionsTaken from '@/components/molecules/RipaActionsTaken'
+import RipaModelMixin from '@/components/mixins/RipaModelMixin.vue'
+import { defaultStop } from '@/utilities/stop.js'
 import { shallowMount } from '@vue/test-utils'
 import Vuetify from 'vuetify'
 
 describe('Ripa Actions Taken', () => {
   let vuetify
   let wrapper
+  let stop
 
   beforeEach(() => {
     vuetify = new Vuetify()
+    stop = defaultStop()
   })
 
   const factory = propsData => {
@@ -16,11 +20,12 @@ describe('Ripa Actions Taken', () => {
       propsData: {
         ...propsData,
       },
+      mixins: [RipaModelMixin],
     })
   }
 
   it('should match snapshot', () => {
-    wrapper = factory()
+    wrapper = factory({ value: stop })
 
     expect(wrapper.html()).toMatchSnapshot()
   })
