2e0d26f94fdea57e445ab6120f898d23e04b98a8
Jake Kellas <jacob.kellas@sdsheriff.org>
2022-01-14 07:45:37 -0800

    Modify table for favorites dialog

--- UI/src/components/molecules/RipaFavoritesGrid.vue
+++ UI/src/components/molecules/RipaFavoritesGrid.vue
@@ -1,78 +1,69 @@
 <template>
   <div class="ripa-favorites-grid">
-    <v-card-title>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
+    <v-text-field
+      v-if="favorites.length > 9"
+      v-model="search"
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+    ></v-text-field>
     <v-data-table
       :loading="loading"
       :headers="headers"
       :items="favorites"
       :search="search"
+      :items-per-page="-1"
+      :hide-default-footer="true"
+      :mobile-breakpoint="0"
       @click:row="handleRowClick"
-      sort-by="name"
       single-select
     >
       <template v-slot:top>
-        <v-toolbar flat>
-          <v-toolbar-title class="tw-uppercase"
-            >Maintain Favorites</v-toolbar-title
-          >
-          <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="500px" persistent>
-            <v-card>
-              <v-card-title>
-                <span>{{ formTitle }}</span>
-              </v-card-title>
+        <v-dialog v-model="dialog" max-width="500px" persistent>
+          <v-card>
+            <v-card-title>
+              <span>{{ formTitle }}</span>
+            </v-card-title>
 
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12">
-                      <v-text-field
-                        v-model="editedItem.name"
-                        label="Favorite Name"
-                        required
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Favorite Name"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
 
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">
-                  Cancel
-                </v-btn>
-                <template v-if="isOnlineAndAuthenticated">
-                  <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
-                </template>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-          <v-dialog v-model="dialogDelete" max-width="500px" persistent>
-            <v-card>
-              <v-card-title
-                >Are you sure you want to delete this favorite?</v-card-title
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+              <template v-if="isOnlineAndAuthenticated">
+                <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+              </template>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px" persistent>
+          <v-card>
+            <v-card-title
+              >Are you sure you want to delete this favorite?</v-card-title
+            >
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">No</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm"
+                >Yes</v-btn
               >
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete"
-                  >No</v-btn
-                >
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm"
-                  >Yes</v-btn
-                >
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </template>
       <template v-slot:item.actions="{ item }">
         <template v-if="isOnlineAndAuthenticated">
@@ -103,7 +94,6 @@ export default {
       dialogDelete: false,
       headers: [
         { text: 'Name', value: 'name' },
-        { text: 'Update Date', value: 'updateDate', width: '150' },
         {
           text: 'Actions',
           value: 'actions',
