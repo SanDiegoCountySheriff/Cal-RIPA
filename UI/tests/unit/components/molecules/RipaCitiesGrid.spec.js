87f164a2a9dedcfb937a4b0f4aef7ca703ce62bd
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2022-02-08 14:17:12 -0800

    Begin RipaCitiesGrid

---
+++ UI/tests/unit/components/molecules/RipaCitiesGrid.spec.js
@@ -0,0 +1,12 @@
+import RipaCitiesGrid from '@/components/molecules/RipaCitiesGrid.vue'
+import { shallowMount } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+
+describe('Ripa Cities Grid',() => {
+    let vuetify
+    let wrapper
+
+    beforeEach(() => {
+        
+    })
+})
