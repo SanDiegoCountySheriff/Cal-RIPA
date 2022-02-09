e61ca78e58b247b70a40402c02a60723d35db5a5
Jake Kellas <jacob.kellas@sdsheriff.org>
2022-02-09 08:15:04 -0800

    Complete RipaCitiesGrid tests

--- UI/tests/unit/components/molecules/RipaCitiesGrid.spec.js
+++ UI/tests/unit/components/molecules/RipaCitiesGrid.spec.js
@@ -2,11 +2,39 @@ import RipaCitiesGrid from '@/components/molecules/RipaCitiesGrid.vue'
 import { shallowMount } from '@vue/test-utils'
 import Vuetify from 'vuetify'
 
-describe('Ripa Cities Grid',() => {
-    let vuetify
-    let wrapper
+describe('Ripa Cities Grid', () => {
+  let vuetify
+  let wrapper
 
-    beforeEach(() => {
-        
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  afterEach(() => {
+    wrapper.destroy()
+  })
+
+  const factory = propsData => {
+    return shallowMount(RipaCitiesGrid, {
+      vuetify,
+      propsData: {
+        ...propsData,
+      },
     })
+  }
+
+  it('should match snapshot', () => {
+    wrapper = factory()
+
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('should watch items', async () => {
+    wrapper = factory()
+
+    wrapper.setProps({ items: ['item1'] })
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.vm.cities).toEqual(['item1'])
+  })
 })
