c0aa3b86147c9573dc5f85ca4d3b00a3c24a1695
Jake Kellas <jacob.kellas@sdsheriff.org>
2022-02-11 10:16:39 -0800

    Finish reasonableSuspicion unit test

--- UI/tests/unit/components/organisms/RipaFormWrapper.spec.js
+++ UI/tests/unit/components/organisms/RipaFormWrapper.spec.js
@@ -1,13 +1,15 @@
 import RipaFormWrapper from '@/components/organisms/RipaFormWrapper.vue'
-import RipaFormStep3 from '@/components/molecules/RipaFormStep3.vue'
-import RipaStopReason from '@/components/molecules/RipaStopReason.vue'
 import RipaSelect from '@/components/atoms/RipaSelect.vue'
-import { createLocalVue, createWrapper, mount } from '@vue/test-utils'
+import RipaCheckGroup from '@/components/atoms/RipaCheckGroup.vue'
+import { createLocalVue, mount } from '@vue/test-utils'
 import { defaultStop } from '@/utilities/stop.js'
 import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+import store from '@/store'
 
 describe('Ripa Form Wrapper', () => {
   const localVue = createLocalVue()
+  localVue.use(Vuex)
   let wrapper
   let vuetify
   let stop
@@ -24,44 +26,105 @@ describe('Ripa Form Wrapper', () => {
   const factory = propsData => {
     return mount(RipaFormWrapper, {
       localVue,
+      store,
       vuetify,
       propsData: {
-        value: stop,
         ...propsData,
       },
     })
   }
 
   it('should match snapshot', () => {
-    wrapper = factory()
+    wrapper = factory({ value: stop })
 
     expect(wrapper.element).toMatchSnapshot()
   })
 
   it('should reset reasonableSuspicion when changing reasonForStop', async () => {
-    wrapper = factory({ formStepIndex: 3 })
+    stop = CreateStartOfFormStepIndexThreeStop(stop)
+    wrapper = factory({ value: stop, formStepIndex: 3 })
     const expected = []
 
-    const nextButton = wrapper
-      .findAll('.v-btn')
-      .filter(btn => btn.text() === 'Next')
-      .at(0)
-
-    const backButton = wrapper
-      .findAll('.v-btn')
-      .filter(btn => btn.text() === 'Back')
-      .at(0)
-
-    const selector = wrapper.find('.v-select').props().items[1]
-    const reasonSelector = mount(wrapper.findComponent(RipaSelect))
-    reasonSelector.vm.selectItem(selector)
+    const selector = wrapper.findComponent(RipaSelect)
+    selector.vm.model = 2
     await wrapper.vm.$nextTick()
-    // wrapper.findAll('.v-select__slot').at(0).trigger('click')
-    // await wrapper.vm.$nextTick()
 
-    // wrapper.findAll('.v-list-item').at(1).trigger('click')
-    // await wrapper.vm.$nextTick()
+    const checkGroup = wrapper.findComponent(RipaCheckGroup)
+    checkGroup.vm.model = [1]
+
+    selector.vm.model = 1
+    await wrapper.vm.$nextTick()
 
     expect(wrapper.vm.stop.stopReason.reasonableSuspicion).toEqual(expected)
   })
 })
+
+function CreateStartOfFormStepIndexThreeStop(stop) {
+  return {
+    ...stop,
+    actionsTaken: {
+      actionsTakenDuringStop: [],
+      personSearchConsentGiven: false,
+      propertySearchConsentGiven: false,
+      basisForSearch: [],
+      basisForSearchExplanation: null,
+      basisForSearchPiiFound: false,
+      propertyWasSeized: false,
+      basisForPropertySeizure: [],
+      typeOfPropertySeized: [],
+      anyContraband: false,
+      contrabandOrEvidenceDiscovered: [],
+    },
+    location: {
+      blockNumber: '0',
+      streetName: 'Anystreet',
+      city: 'SAN DIEGO',
+      beat: '009',
+    },
+    person: {
+      anyDisabilities: false,
+      isStudent: false,
+      perceivedAge: 10,
+      perceivedGender: 1,
+      genderNonconforming: false,
+      perceivedLimitedEnglish: false,
+      perceivedLgbt: false,
+      perceivedOrKnownDisability: [],
+      perceivedRace: [1],
+    },
+    stopDate: {
+      duration: 10,
+      stopInResponseToCfs: false,
+    },
+    stopReason: {
+      reasonForStop: null,
+      educationViolation: null,
+      educationViolationCode: null,
+      trafficViolation: null,
+      trafficViolationCode: null,
+      reasonableSuspicion: [],
+      reasonableSuspicionCode: null,
+      searchOfPerson: null,
+      reasonForStopExplanation: null,
+      reasonForStopPiiFound: false,
+    },
+    stopResult: {
+      resultsOfStop2: false,
+      resultsOfStop3: false,
+      resultsOfStop4: false,
+      resultsOfStop5: false,
+      resultsOfStop6: false,
+      resultsOfStop7: false,
+      resultsOfStop8: false,
+      resultsOfStop9: false,
+      resultsOfStop10: false,
+      resultsOfStop11: false,
+      resultsOfStop12: false,
+      resultsOfStop13: false,
+      warningCodes: [],
+      citationCodes: [],
+      infieldCodes: [],
+      custodialArrestCodes: [],
+    },
+  }
+}
