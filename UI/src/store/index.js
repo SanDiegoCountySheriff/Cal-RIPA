7a455bb56d46050ca2e6f7fc3275951cbd817a22
Jake Kellas <jacob.kellas@sdsheriff.org>
2022-03-04 14:35:01 -0800

    code cleanup

--- UI/src/store/index.js
+++ UI/src/store/index.js
@@ -428,34 +428,6 @@ export default new Vuex.Store({
           oid: value.idTokenClaims.oid,
         }
       }
-
-      // if (value && value.profile) {
-      //   let isAnAdmin = false
-      //   const roles = value.profile?.roles || []
-      //   isAnAdmin = roles.filter(roleObj => {
-      //     return roleObj === 'RIPA-ADMINS-ROLE'
-      //   })
-      //   let fullName
-      //   const firstName = value.profile.given_name
-      //   const lastName = value.profile.family_name
-
-      //   if (state.apiConfig.useOfficerUpn) {
-      //     fullName = value.profile.upn.split('@')[0]
-      //   } else {
-      //     fullName = `${firstName} ${lastName}`
-      //   }
-
-      //   state.user = {
-      //     ...state.user,
-      //     email: value.profile.email,
-      //     firstName,
-      //     fullName,
-      //     isAdmin: isAnAdmin.length > 0,
-      //     isAuthenticated: true,
-      //     lastName,
-      //     oid: value.profile.oid,
-      //   }
-      // }
     },
     updateUserProfile(state, value) {
       let yearsExperience = value.yearsExperience
