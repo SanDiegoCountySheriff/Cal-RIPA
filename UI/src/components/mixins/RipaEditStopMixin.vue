9cbc74a109bc6b69dde0f75b7e4a946eb80d51ab
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2022-03-10 07:47:58 -0800

    Remove admin editing from EditStopMixin

--- UI/src/components/mixins/RipaEditStopMixin.vue
+++ UI/src/components/mixins/RipaEditStopMixin.vue
@@ -45,7 +45,6 @@ export default {
       const stop = fullStopToStop(fullStop)
 
       localStorage.setItem('ripa_form_step_index', '7')
-      localStorage.setItem('ripa_form_admin_editing', '1')
       localStorage.setItem('ripa_form_editing', '1')
       localStorage.setItem('ripa_form_edit_route', route)
       localStorage.setItem('ripa_form_stop', JSON.stringify(stop))
