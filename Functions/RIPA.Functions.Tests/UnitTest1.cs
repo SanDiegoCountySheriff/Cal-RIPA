53d1d121e3eab192ddde79c375d7a59da3f94dd7
Jake Kellas <jacob.kellas@sdsheriff.org>
2022-04-15 12:19:59 -0700

    Update UnitTest1.cs

--- Functions/RIPA.Functions.Tests/UnitTest1.cs
+++ Functions/RIPA.Functions.Tests/UnitTest1.cs
@@ -5,8 +5,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Primitives;
 using Moq;
+using RIPA.Functions.Common.Models;
 using RIPA.Functions.Common.Services.Stop.CosmosDb;
 using RIPA.Functions.Stop.Functions;
+using RIPA.Functions.Submission.Services.REST;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
@@ -44,6 +46,84 @@ namespace RIPA.Functions.Tests
             var queryParams = new Dictionary<string, StringValues>() { { "number", obj.ToString() } };
             request.Query = new QueryCollection(queryParams);
             return request;
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var stopService = new StopService();
+            var stop = CreateDefaultStop();
+
+            stopService.CastToDojStop(stop);
+        }
+
+        private Common.Models.Stop CreateDefaultStop()
+        {
+            return new Common.Models.Stop()
+            {
+                Id = "1",
+                Ori = "001",
+                Agency = "Agency",
+                OfficerId = "1",
+                OfficerName = "Officer Name",
+                ExpYears = "1",
+                OfficerAssignment = new OfficerAssignment()
+                {
+                    Key = "1",
+                    Type = "10",
+                    OtherType = "Other Type",
+                },
+                Date = "2021, 01, 01",
+                Time = "12:00PM",
+                Location = new Location()
+                {
+                    ToggleLocationOptions = false,
+                    BlockNumber = "1000",
+                    StreetName = "AnyStreet St.",
+                    City = new City()
+                    {
+                        Codes = new Codes()
+                        {
+                            Code = "1",
+                            Text = "City",
+                        }
+                    },
+                    School = false,
+                    OutOfCounty = false,
+                    PiiFound = false,
+                    GeoLocation = new GeoLocation()
+                    {
+                        Latitude = 1,
+                        Longitude = 1
+                    }
+                },
+                StopDuration = 10,
+                StopInResponseToCFS = false,
+                ListPersonStopped = new PersonStopped[] {
+                    new PersonStopped() { }
+                },
+                ListSubmission = new Common.Models.Submission[]
+                {
+                    new Common.Models.Submission() { }
+                },
+                Status = "Active",
+                IsPiiFound = false,
+                PiiEntities = new PiiEntity[]
+                {
+                    new PiiEntity() { }
+                },
+                OverridePii = false,
+                EditStopExplanation = "EditStopExplanation",
+                IsEdited = false,
+                Telemetry = new Telemetry()
+                {
+
+                },
+                ListAgencyQuestion = new AgencyQuestion[]
+                {
+                    new AgencyQuestion() { }
+                }
+            };
         }
     }
 }
