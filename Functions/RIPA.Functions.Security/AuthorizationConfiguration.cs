452b10815ea4c61c1817f3728198849999027eee
Jake Kellas <jacob.kellas@sdsheriff.org>
2023-02-22 06:54:39 -0800

    fix: fix namespace

--- Functions/RIPA.Functions.Security/AuthorizationConfiguration.cs
+++ Functions/RIPA.Functions.Security/AuthorizationConfiguration.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using System.Globalization;
 
-namespace RIPA.Functions.Security;
-
-public static class AuthorizationConfiguration
+namespace RIPA.Functions.Security
 {
-    internal static readonly string RIPAAppClientID = Environment.GetEnvironmentVariable("ripa_app_id");
-    internal static readonly string RIPATenantId = Environment.GetEnvironmentVariable("ripa_tenant_id");
-    internal static readonly string RIPATenantName = Environment.GetEnvironmentVariable("ripa_tenant_name");
+    public static class AuthorizationConfiguration
+    {
+        internal static readonly string RIPAAppClientID = Environment.GetEnvironmentVariable("ripa_app_id");
+        internal static readonly string RIPATenantId = Environment.GetEnvironmentVariable("ripa_tenant_id");
+        internal static readonly string RIPATenantName = Environment.GetEnvironmentVariable("ripa_tenant_name");
 
-    internal static readonly string RIPALoginUrl = "https://login.microsoftonline.com/{0}/v2.0";
-    internal static readonly string RIPAAutorityName = string.Format(CultureInfo.InvariantCulture, RIPALoginUrl, RIPATenantName);
-    internal static readonly List<string> RIPAValidIssuers = new List<string>()
+        internal static readonly string RIPALoginUrl = "https://login.microsoftonline.com/{0}/v2.0";
+        internal static readonly string RIPAAutorityName = string.Format(CultureInfo.InvariantCulture, RIPALoginUrl, RIPATenantName);
+        internal static readonly List<string> RIPAValidIssuers = new List<string>()
         {
             $"https://login.microsoftonline.com/{RIPATenantName}/",
             $"https://login.microsoftonline.com/{RIPATenantId}/",
@@ -26,4 +26,5 @@ public static class AuthorizationConfiguration
             $"https://login.microsoft.com/{RIPATenantId}/",
             $"https://sts.windows.net/{RIPATenantId}/"
         };
+    }
 }
