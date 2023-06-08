c7e05b7c971b96e085349c9c7db72bbff5cee716
Jake Kellas <83732510+jacobkellas@users.noreply.github.com>
2023-06-08 08:31:41 -0700

    Update babel.config.js

--- UI/babel.config.js
+++ UI/babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties'],
-    ['@babel/plugin-proposal-private-methods'],
-    ['@babel/plugin-proposal-private-property-in-object'],
-    ['@babel/plugin-proposal-optional-chaining'],
-    ['@babel/plugin-proposal-nullish-coalescing-operator'],
-  ],
+  // plugins: [
+  //   ['@babel/plugin-proposal-class-properties'],
+  //   ['@babel/plugin-proposal-private-methods'],
+  //   ['@babel/plugin-proposal-private-property-in-object'],
+  //   ['@babel/plugin-proposal-optional-chaining'],
+  //   ['@babel/plugin-proposal-nullish-coalescing-operator'],
+  // ],
 }
