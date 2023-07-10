0c94666a49698fe464c71dba96e53fea5599c80b
Alyssa Arce <aarcexsh@sdsheriff.com>
2023-07-10 14:13:39 -0700

    refactor: Include mandatory text input explanation for Basis for Search(parole/probation/PRCS/mandatory supervision)

--- UI/src/components/molecules/RipaActionsTaken.vue
+++ UI/src/components/molecules/RipaActionsTaken.vue
@@ -342,12 +342,12 @@ export default {
         return false
       }
 
-      if (
-        this.model.actionsTaken.basisForSearch.length === 1 &&
-        this.model.actionsTaken.basisForSearch.includes(4)
-      ) {
-        return false
-      }
+      // if (
+      //   this.model.actionsTaken.basisForSearch.length === 1 &&
+      //   this.model.actionsTaken.basisForSearch.includes(4)
+      // ) {
+      //   return false
+      // }
 
       return this.wasSearchOfPersonOrPropertyConducted
     },
