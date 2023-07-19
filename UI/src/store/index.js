d4e760b265b153788143a06e18958aeb51e46b5c
Jake Kellas <jacob.kellas@sdsheriff.org>
2023-07-19 09:35:29 -0700

    chore: revert local testing

--- UI/src/store/index.js
+++ UI/src/store/index.js
@@ -923,7 +923,7 @@ export default new Vuex.Store({
       commit('updateStopSubmissionStatusTotal', 1)
       return axios
         .put(
-          `http://localhost:7071/api/v${stop.stopVersion}/PutStop/${stop.id}`,
+          `${state.apiConfig.apiBaseUrl}stop/v${stop.stopVersion}/PutStop/${stop.id}`,
           stop,
           {
             headers: {
@@ -1378,7 +1378,7 @@ export default new Vuex.Store({
 
       return axios
         .get(
-          `http://localhost:7071/api/v${state.version}/GetStops?officerId=${officerId}&limit=10`,
+          `${state.apiConfig.apiBaseUrl}stop/v${state.version}/GetStops?officerId=${officerId}&limit=10`,
           {
             headers: {
               'Ocp-Apim-Subscription-Key': state.apiConfig.apiSubscription,
@@ -1468,7 +1468,7 @@ export default new Vuex.Store({
       }
       return axios
         .get(
-          `http://localhost:7071/api/v${state.version}/GetStops${queryString}`,
+          `${state.apiConfig.apiBaseUrl}stop/v${state.version}/GetStops${queryString}`,
           {
             headers: {
               'Ocp-Apim-Subscription-Key': state.apiConfig.apiSubscription,
@@ -1494,7 +1494,7 @@ export default new Vuex.Store({
     getAdminStopAudits({ state }, stopId) {
       return axios
         .get(
-          `http://localhost:7071/api/v${state.version}/GetStopAudits?id=${stopId}`,
+          `${state.apiConfig.apiBaseUrl}stop/v${state.version}/GetStopAudits?id=${stopId}`,
           {
             headers: {
               'Content-Type': 'application/json',
@@ -1663,7 +1663,7 @@ export default new Vuex.Store({
     getErrorCodes({ commit, state }, value) {
       return axios
         .get(
-          `http://localhost:7071/api/v${state.version}/GetErrorCodes?search=${value}`,
+          `${state.apiConfig.apiBaseUrl}stop/v${state.version}/GetErrorCodes?search=${value}`,
           {
             headers: {
               'Ocp-Apim-Subscription-Key': state.apiConfig.apiSubscription,
