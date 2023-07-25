58fcb393e3433270eaa030e2addf47636c8a3ec7
Frida Zavala Hernandez <101593669+FridaPZavalaHernandez@users.noreply.github.com>
2023-07-25 10:49:04 -0700

    added-unit-test-for-insideResidence-and-passengerInVehicle

--- UI/tests/unit/components/molecules/RipaStopReason.spec.js
+++ UI/tests/unit/components/molecules/RipaStopReason.spec.js
@@ -3,6 +3,7 @@ import { shallowMount, mount } from '@vue/test-utils'
 import { defaultStop } from '@/utilities/stop'
 import Vuetify from 'vuetify'
 import { computed } from 'vue'
+import { V2_STOP } from '../../constants/RipaFormContainerTestConstants'
 
 describe('Ripa Stop Reason', () => {
   let vuetify
@@ -48,5 +49,63 @@ describe('Ripa Stop Reason', () => {
     })
 
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('stop type is vechiular show this question "Passenger in vehicle"', () => {
+    const updatedStop = V2_STOP
+    updatedStop.stopType = 'Vehicular'
+
+    wrapper = mount(RipaStopReason, {
+      vuetify,
+      propsData: { value: V2_STOP },
+      provide: {
+        loadingPiiStep3: computed(() => false),
+        isOnlineAndAuthenticated() {
+          return true
+        },
+        lastReason() {
+          return {}
+        },
+        personSearchAutomaticallySelected() {
+          return false
+        },
+        propertySearchAutomaticallySelected() {
+          return false
+        },
+        statutes: computed(() => []),
+      },
+    })
+
+    expect(wrapper.html()).toContain('passenger in a vehicle')
+    expect(wrapper.html()).not.toContain('person in residence')
+  })
+
+  it('stop type is vechiular show this question "Person was inside a residence"', () => {
+    const updatedStop = V2_STOP
+    updatedStop.stopType = 'Pedestrian'
+
+    wrapper = mount(RipaStopReason, {
+      vuetify,
+      propsData: { value: V2_STOP },
+      provide: {
+        loadingPiiStep3: computed(() => false),
+        isOnlineAndAuthenticated() {
+          return true
+        },
+        lastReason() {
+          return {}
+        },
+        personSearchAutomaticallySelected() {
+          return false
+        },
+        propertySearchAutomaticallySelected() {
+          return false
+        },
+        statutes: computed(() => []),
+      },
+    })
+
+    expect(wrapper.html()).toContain('person was inside a residence')
+    expect(wrapper.html()).not.toContain('passenger in a vehicle')
   })
 })
