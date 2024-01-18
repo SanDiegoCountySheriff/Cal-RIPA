4be813dffee8fa87d3d752dbd85a7ac74efd42af
Jake Kellas <jacob.kellas@sdsheriff.org>
2024-01-18 14:56:27 -0800

    fix: revert local testing

--- UI/src/store/index.js
+++ UI/src/store/index.js
@@ -719,7 +719,7 @@ export default new Vuex.Store({
       }
       return axios
         .put(
-          `http://localhost:7072/api/v2/PutUser/${updatedUser.id}`,
+          `${state.apiConfig.apiBaseUrl}userprofile/v2/PutUser/${updatedUser.id}`,
           user,
           {
             headers: {
@@ -743,7 +743,7 @@ export default new Vuex.Store({
       formData.append('file', usersFile)
       return axios
         .post(
-          `http://localhost:7072/api/v2/PostUpload?agency=${usersAgency}`,
+          `${state.apiConfig.apiBaseUrl}userprofile/v2/PostUpload?agency=${usersAgency}`,
           formData,
           {
             headers: {
@@ -915,7 +915,7 @@ export default new Vuex.Store({
 
       return axios
         .put(
-          `http://localhost:7072/api/v2/PutUser/${userId}`,
+          `${state.apiConfig.apiBaseUrl}userprofile/v2/PutUser/${userId}`,
           user,
           {
             headers: {
@@ -1422,7 +1422,7 @@ export default new Vuex.Store({
 
     getAdminUsers({ commit, state }) {
       return axios
-        .get(`http://localhost:7072/api/v2/GetUsers`, {
+        .get(`${state.apiConfig.apiBaseUrl}userprofile/v2/GetUsers`, {
           headers: {
             'Ocp-Apim-Subscription-Key': `${state.apiConfig.apiSubscription}`,
             'Cache-Control': 'no-cache',
@@ -1697,7 +1697,7 @@ export default new Vuex.Store({
     getUser({ commit, state, getters }) {
       const id = state.user.oid
       return axios
-        .get(`http://localhost:7072/api/v2/GetUser/${id}`, {
+        .get(`${state.apiConfig.apiBaseUrl}userprofile/v2/GetUser/${id}`, {
           headers: {
             'Ocp-Apim-Subscription-Key': state.apiConfig.apiSubscription,
             'Cache-Control': 'no-cache',
